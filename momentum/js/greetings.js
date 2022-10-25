0d8769ae01dceb94dc3189edc4d4cf8d69ff271e
Teddy <wjddns363@naver.com>
2022-10-25 11:42:36 +0900

    폴더분리, 시계만들기

--- momentum/js/greetings.js
+++ momentum/js/greetings.js
@@ -7,12 +7,11 @@ const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
     paintGreetings(username);
 }
-
 
 function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
@@ -24,7 +23,6 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 if(savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
-
 } else {
     paintGreetings(savedUsername);
 }
